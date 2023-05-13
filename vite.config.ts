e584579dd6481b58e3584d2e03b172caa8d2b2e4
a.orlovets <orlovetsjob@gmail.com>
2023-05-13 19:42:32 +0300

    update: config

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,10 @@ import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    UnoCSS({ configFile: './uno.config.ts' })
-  ],
+  plugins: [vue(), UnoCSS()],
   resolve: {
     alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
-    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
+    extensions: ['.js', '.jsx', '.json', '.cjs', '.mjs', '.ts', '.tsx', '.vue'],
     preserveSymlinks: true
   },
   define: { 'process.env': {}},
